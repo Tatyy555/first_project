878db7a6d9f3455fd9210a6130ab06f197a9a890
okazhiko <o.kazhiko@gmail.com>
2022-07-05 00:39:27 +0900

    add constants.dart

---
+++ first_project/lib/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const Color kMainColor = Color(0xFF283B96);
+const Color kAccentColor = Color(0xFFD9BA1E);
+const Color kBaseColor = Color(0xFFFEFEFE);
+const Color kTextColor = Color(0xFF000000);
+const Color kShadowColor = Color(0xFFD9D9D9);
