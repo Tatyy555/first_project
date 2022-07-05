015f0bcd58f165bb5086372b30228be22a1e158d
okazhiko <o.kazhiko@gmail.com>
2022-07-05 23:12:52 +0900

    modify design

--- first_project/lib/newsaddpage.dart
+++ first_project/lib/newsaddpage.dart
@@ -1,32 +1,30 @@
 import 'package:flutter/material.dart';
-
 // Firestoreと連携させるため以下追加。
 import 'package:cloud_firestore/cloud_firestore.dart';
-
 // Firebase authと連携させるため以下追加。
 import 'package:firebase_auth/firebase_auth.dart';
-
 // hashtag機能を実装。
 import 'package:material_tag_editor/tag_editor.dart';
+// constants.dart
+import 'constants.dart';
 
 // ニュース追加画面用のWidget。
-class NewsAddPage extends StatefulWidget{
- 
+class NewsAddPage extends StatefulWidget {
   // 引数からユーザー情報を受け取る
   const NewsAddPage(this.user);
   // ユーザー情報
   final User user;
-  
+
   @override
   _NewsAddPageState createState() => _NewsAddPageState();
 }
 
-class _NewsAddPageState extends State<NewsAddPage>{
+class _NewsAddPageState extends State<NewsAddPage> {
   // 入力されたテキストをデータとして持つ。
   String url = '';
-  String hash = ''; 
-  String comment = '';  
-  
+  String hash = '';
+  String comment = '';
+
   // hashtag用の情報。
   final List<String> _values = [];
   final FocusNode _focusNode = FocusNode();
@@ -38,128 +36,204 @@ class _NewsAddPageState extends State<NewsAddPage>{
     });
   }
 
-
-  @override 
-  Widget build(BuildContext context){
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        padding: const EdgeInsets.all(40),
-        child:Column(
-          children: <Widget>[
-            Expanded(
-              flex: 2, // 割合.
-              child:TextFormField(
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'URL',
-                  labelText: 'URL',
-                ),
-                maxLines: 3,
-                onChanged: (String value) {
-                  setState(() {
-                    url = value;
-                  });
-                }
-              ),
-            ),
-
-            Expanded(
-              flex: 4, // 割合.
-              child:TextFormField(
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'コメント',
-                  labelText: 'コメント',
-                ),
-                maxLines: 10,
-                onChanged: (String value) {
-                  setState(() {
-                    comment = value;
-                  });
-                }
-              ),
-            ),
-
-            TagEditor(
-              length: _values.length,
-              controller: _textEditingController,
-              focusNode: _focusNode,
-              delimiters: const [',', ' '],
-              hasAddButton: true,
-              resetTextOnSubmitted: true,
-              // This is set to grey just to illustrate the `textStyle` prop
-              textStyle: const TextStyle(color: Colors.grey),
-              onSubmitted: (outstandingValue) {
-                setState(() {
-                  _values.add(outstandingValue);
-                });
-              },
-              inputDecoration: const InputDecoration(
-                border: InputBorder.none,
-                hintText: 'タグを設定してください',
-              ),
-              onTagChanged: (newValue) {
-                setState(() {
-                  _values.add(newValue);
-                });
-              },
-              tagBuilder: (context, index) => _Chip(
-                index: index,
-                label: _values[index],
-                onDeleted: _onDelete,
-              ),
-            ),
-            Expanded(
-              flex: 2, // 割合.
-              child: Row(
-                // 余白を均等に並べる。
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children:<Widget>[
-                  SizedBox(
-                    width: 130,
-                    // 登録ボタン
-                    child: ElevatedButton(
-                      child: const Text('登録'),
-                      onPressed: () async {
-                        // 現在の日時
-                        final date = DateTime.now().toLocal().toIso8601String(); 
-                        // NewsAddPageのデータを参照
-                        final email = widget.user.email; 
-                         // 登録用ドキュメント作成
-                        await FirebaseFirestore.instance
-                        .collection('news') // コレクションID指定
-                        .doc() // ドキュメントID自動生成
-                        .set({
-                          'url': url,
-                          'hash': hash,    
-                          'comment': comment,
-                          'email': email,
-                          'date': date
-                        });
-                        Navigator.of(context).pop();
-                      },
-                    ),
+        backgroundColor: kMainColor,
+        body: SafeArea(
+          bottom: false,
+          child: Column(
+            children: [
+              Container(
+                height: 100,
+                padding: EdgeInsets.all(20),
+                child: Text(
+                  'ニュース登録',
+                  style: TextStyle(
+                    color: kBaseColor,
+                    fontSize: 30,
                   ),
-                  SizedBox(
-                    width: 120,
-                    // 破棄ボタン
-                    child: ElevatedButton(
-                      child: const Text('破棄'),
-                      onPressed: () async {
-                        // 前の画面に戻るだけです（何もデータは渡さない。）
-                        Navigator.of(context).pop();
-                      },
-                    ),
-                  ),
-                ]
+                ),
               ),
-            ),
-          ]
-        )
-      )
-    );
+              Expanded(
+                  child: Container(
+                      padding: const EdgeInsets.symmetric(horizontal: 40),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(50),
+                          topRight: Radius.circular(50),
+                        ),
+                        color: kBaseColor,
+                      ),
+                      child: Column(children: <Widget>[
+                        SizedBox(
+                          height: 40,
+                        ),
+                        Container(
+                          width: double.infinity,
+                          child: Text(
+                            'URL',
+                            style: TextStyle(
+                                color: kAccentColor,
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                        TextFormField(
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(15),
+                              ),
+                            ),
+                            maxLines: 1,
+                            onChanged: (String value) {
+                              setState(() {
+                                url = value;
+                              });
+                            }),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Container(
+                          width: double.infinity,
+                          child: Text(
+                            'ハッシュタグ',
+                            style: TextStyle(
+                                color: kAccentColor,
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                        TagEditor(
+                          length: _values.length,
+                          controller: _textEditingController,
+                          focusNode: _focusNode,
+                          delimiters: const [',', ' '],
+                          hasAddButton: true,
+                          resetTextOnSubmitted: true,
+                          // This is set to grey just to illustrate the `textStyle` prop
+                          textStyle: const TextStyle(color: Colors.grey),
+                          onSubmitted: (outstandingValue) {
+                            setState(() {
+                              _values.add(outstandingValue);
+                            });
+                          },
+                          inputDecoration: InputDecoration(
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(15),
+                            ),
+                            hintText: 'タグを設定してください',
+                          ),
+                          onTagChanged: (newValue) {
+                            setState(() {
+                              _values.add(newValue);
+                            });
+                          },
+                          tagBuilder: (context, index) => _Chip(
+                            index: index,
+                            label: _values[index],
+                            onDeleted: _onDelete,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Container(
+                          width: double.infinity,
+                          child: Text(
+                            'コメント',
+                            style: TextStyle(
+                                color: kAccentColor,
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                        TextFormField(
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(15)),
+                            ),
+                            maxLines: 10,
+                            onChanged: (String value) {
+                              setState(() {
+                                comment = value;
+                              });
+                            }),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Row(
+                            // 余白を均等に並べる。
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: <Widget>[
+                              SizedBox(
+                                width: 130,
+                                height: 60,
+                                // 登録ボタン
+                                child: ElevatedButton(
+                                  style: ElevatedButton.styleFrom(
+                                      primary: kMainColor,
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(70),
+                                      )),
+                                  child: const Text(
+                                    '登録',
+                                    style: TextStyle(
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                  onPressed: () async {
+                                    // 現在の日時
+                                    final date = DateTime.now()
+                                        .toLocal()
+                                        .toIso8601String();
+                                    // NewsAddPageのデータを参照
+                                    final email = widget.user.email;
+                                    // 登録用ドキュメント作成
+                                    await FirebaseFirestore.instance
+                                        .collection('news') // コレクションID指定
+                                        .doc() // ドキュメントID自動生成
+                                        .set({
+                                      'url': url,
+                                      'hash': hash,
+                                      'comment': comment,
+                                      'email': email,
+                                      'date': date
+                                    });
+                                    Navigator.of(context).pop();
+                                  },
+                                ),
+                              ),
+                              SizedBox(
+                                width: 130,
+                                height: 60,
+                                // 破棄ボタン
+                                child: ElevatedButton(
+                                  style: ElevatedButton.styleFrom(
+                                      primary: kMainColor,
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(70),
+                                      )),
+                                  child: const Text(
+                                    '破棄',
+                                    style: TextStyle(
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                  onPressed: () async {
+                                    // 前の画面に戻るだけです（何もデータは渡さない。）
+                                    Navigator.of(context).pop();
+                                  },
+                                ),
+                              ),
+                            ]),
+                      ])))
+            ],
+          ),
+        ));
   }
 }
+
 class _Chip extends StatelessWidget {
   const _Chip({
     required this.label,
